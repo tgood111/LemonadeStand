cfda37b4b4b4e7cbba7363ed8348f7d34913eb02
tgood111 <tgood111@live.com>
2017-08-14 20:24:45 -0500

    Created an Inventory and added item class.

--- LemonadeStand/Inventory.cs
+++ LemonadeStand/Inventory.cs
@@ -6,9 +6,35 @@ using System.Threading.Tasks;
 
 namespace LemonadeStand
 {
-    class Inventory
+    public class Inventory
     {
-        int lemonCount;
+        public Item lemon;
+        public Item sugar;
+        public Item ice;
+        public Item cups;
 
+        public Inventory()
+        {
+            lemon = new LemonadeStand.Item("Lemon", 0.25);
+            sugar = new LemonadeStand.Item("Sugar", 0.3);
+            ice = new LemonadeStand.Item("Ice", 0.1);
+            cups = new LemonadeStand.Item("Cups", 0.1);
+        }
+
+        public Inventory(double l, double s, double i, double c)
+        {
+            lemon = new LemonadeStand.Item("Lemon", l);
+            sugar = new LemonadeStand.Item("Sugar", s);
+            ice = new LemonadeStand.Item("Ice", i);
+            cups = new LemonadeStand.Item("Cups", c);
+        }
+
+        public void setPrices(double l, double s, double i, double c)
+        {
+            lemon.cost = l;
+            sugar.cost = s;
+            ice.cost = i;
+            cups.cost = c;
+        }
     }
 }
---
+++ LemonadeStand/Item.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    public class Item
+    {
+        public string name { get; set; }
+        public double cost { get; set; }
+        public int amount { get; set; }
+
+        private Item()
+        {
+            // Left Intentionally Blank
+        }
+
+        public Item(string name, double cost)
+        {
+            this.name = name;
+            this.cost = cost;
+            this.amount = 0;
+        }
+    }
+}

02883c985d9a7f4ec647418c581d319355433e0f
tgood111 <tgood111@live.com>
2017-08-14 20:53:08 -0500

    Setup store functions

--- LemonadeStand/Store.cs
+++ LemonadeStand/Store.cs
@@ -6,60 +6,33 @@ using System.Threading.Tasks;
 
 namespace LemonadeStand
 {
-    class Store
+    public class Store
     {
-        Inventory inventory;
-        public int lemon;
-        public int sugar;
-        public int ice;
-        public int cups;
+        public Inventory stock { get; private set; }
+
         public Store()
         {
-
+            stock = new Inventory();
+            restock();
         }
-        public void getLemonCount(Player player)
-        {   switch (lemonCount)
-            {
-                case "10":
-                    if (player == 10)
-                    {
-                        charge .95;
 
-                    }
-                
-            }
-    else if (player choose 30) {
-                (charge 2.08);
-            }
-        }   else if (Customer choose 75 {
-        (player 4.42);
-   
-    public void sugarCount()
-    if (player choose 8) 
-    {
-    (charge customer 1.52)
-} else if (player choose 48)
-{ (charge 3.44);
-    }
-    }
+        public Store(double l, double s, double i, double c)
+        {
+            stock = new Inventory(l, s, i, c);
+            restock();
+        }
 
+        public void restock()
+        {
+            stock.cups.amount = 300;
+            stock.ice.amount = 300;
+            stock.lemon.amount = 300;
+            stock.sugar.amount = 300;
+        }
+
+        public void setPrices(double l, double s, double i, double c)
+        {
+            stock.setPrices(l, s, i, c);
+        }
+    }
 }
-
-
-    public void iceCount()
-    if (player choose 100){
-    (charge .80)
-    else if (Customer choose 250) {
-    (charge 2.03)
-    else if (Customer choose 500){
-    (charge 3.52)
-
-    public void cups()
-    if (player choose 25){
-    (charge .84)
-    }
-    else if (player choose 50)
-{
-    (charge 1.55) }
-    else if (player choose 100){
-(charge 2.89)

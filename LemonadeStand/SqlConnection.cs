8a451eee7e1e279bb4a6784d86e13b54c0cc9fa6
tgood111 <tgood111@live.com>
2017-10-27 12:09:49 -0500

    added code related to database usage in SQLConnection class

--- LemonadeStand/SqlConnection.cs
+++ LemonadeStand/SqlConnection.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LemonadeStand
 {
-    class SqlConnection
+    //the class name had to be chnaged because SqlConnection is the main Microsoft database 
+    class SqlConnection2
     {
+        //first the connection string
+        //use @ symbol to get all the special characters
+
+        string connectionString = @"Data Source=(LocalDb)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\db1.mdf;Initial Catalog=aspnet-WebApplication1-20171026012010;Integrated Security=True";
+
+        // the constructor
+        public SqlConnection2()
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                //do SQL queries here
+
+                connection.Close();
+            }
+        }
     }
 }
